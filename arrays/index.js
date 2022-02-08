e46d07d4a01736ae3fa9200973c6feb4faa486a0
SanchitYadav2005 <sanchit2005yudavnashi@gmail.com>
2022-02-08 12:39:50 +0530

    completed the basics of arrays and learned some arryaus methods

--- arrays/index.js
+++ arrays/index.js
@@ -20,3 +20,25 @@ barbell.push(2.5);
 console.log(barbell);
 const storedValue = barbell.pop();
 console.log(storedValue);
+
+
+// shift and unshift methods
+const movieQeue = ["sanchit", "navneet", "rohit", "dabbu"]
+let enteries = movieQeue.shift();
+console.log(enteries);
+let newVipEnteries = movieQeue.unshift("shreya");
+console.log(movieQeue)
+console.log(Array.isArray(movieQeue))
+
+// nested arrays
+
+const playerScore =[
+    [120,90,40],
+    [50,100,40],
+    [110,89,90]
+]
+
+console.log(playerScore)
+console.log(playerScore[0][0])
+console.log(playerScore[1][1])
+console.log(playerScore[2][2])
