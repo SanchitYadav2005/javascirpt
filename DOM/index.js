d199c7b083c5c60258cf1f3f3f7900b256490a13
SanchitYadav2005 <sanchit2005yudavnashi@gmail.com>
2022-02-28 04:25:14 +0530

    done with js selectors

---
+++ DOM/index.js
@@ -0,0 +1,12 @@
+// this is how we can select elements using javaScript.
+
+
+console.log(document.getElementsByClassName('main'))
+console.log(document.getElementsByTagName('p'))
+console.log(document.getElementById('second'))
+
+
+// query selector
+
+console.log(document.querySelector('div p'))
+console.log(document.querySelectorAll('div'))
